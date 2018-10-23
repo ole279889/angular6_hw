d1b413e3ec1f3fc2228008e6aac38edea26e6d2a
burmitskyoa <ole27988967@yandex.ru>
2018-10-23 17:10:00 +0400

    min project + lesson

---
+++ ang6HW/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import {AfterViewInit, Component, ContentChild, ElementRef, OnInit, ViewChild, ViewChildren} from '@angular/core';
+import {UserListItemComponent} from './user/user-list-item/user-list-item.component';
+import {User} from './shared/user.model';
+
+const LOCAL_STORAGE_KEY = 'users';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  currentUser: User = null;
+  users: User[];
+
+  constructor() {
+    this.users = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
+    this.currentUser = this.getCurrentUser();
+  }
+
+  addUser() {
+    this.users.push(this.currentUser);
+    this.currentUser = this.getCurrentUser();
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.users));
+  }
+
+  getCurrentUser() {
+    return new User(this.users.length, '', '');
+  }
+}
---
+++ ang6HW/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { UserListItemComponent } from './user/user-list-item/user-list-item.component';
+import { UserFormComponent } from './user/user-form/user-form.component';
+import {FormsModule} from '@angular/forms';
+import { UserCardComponent } from './user/user-card/user-card.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserListItemComponent,
+    UserFormComponent,
+    UserCardComponent,
+    UserListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
